45f7902ef22a0818f3489664534ab4028a4a4329
Than Van Pha <phatv90@gmail.com>
2026-01-26 15:43:05 +0900

    Update again

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,6 @@
 # exit when any command fails
 set -e
 
-#Install required Python packages
-sudo python3 -m pip install --no-cache-dir requests pytz beautifulsoup4
-
 # create a tun device if not exist
 # allow passing device to ensure compatibility with Podman
 if [ ! -e /dev/net/tun ]; then
